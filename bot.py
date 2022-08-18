802f5e44d15b1db53c5dd773e1f1d045b977b287
olegatorh <herston4ik@gmail.com>
2022-08-18 14:53:20 +0300

    first release

---
+++ bot.py
@@ -0,0 +1,47 @@
+import asyncio
+import aioschedule
+from aiogram import Bot, Dispatcher, types
+import logging
+from aiogram.utils import executor, exceptions
+
+from bs import data_from_lxml_using_bs4
+
+API_TOKEN = '5782261045:AAHQu702M4dxeIbkc5wKkaLeU4ELZzMUsdo'
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+data = []
+
+
+async def send_message():
+    if len(data) == 0:
+        data.extend(data_from_lxml_using_bs4())
+        for i in data_from_lxml_using_bs4():
+            await bot.send_message('-1001649789391', i)
+    else:
+        unique = (list(set(data) - set(data_from_lxml_using_bs4())))
+        if len(unique) > 0:
+            data.extend(unique)
+            for i in unique:
+                await bot.send_message('-1001649789391', i)
+
+
+@dp.errors_handler(exception=exceptions.RetryAfter)
+async def exception_handler(update: types.Update, exception: exceptions.RetryAfter):
+    await bot.send_message('-1001335100202', "wait some time")
+    return True
+
+
+async def scheduler():
+    aioschedule.every(15).seconds.do(send_message)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
+
+
+async def on_startup(_):
+    asyncio.create_task(scheduler())
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
---
+++ bs.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def data_from_lxml_using_bs4():
+    url = "https://www.tesmanian.com/blogs/tesmanian-blog"
+    data = []
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'lxml')
+    title_and_link = soup.select('h2 a')
+    for i in title_and_link:
+        data.append(f"https://www.tesmanian.com{i.attrs['href']}")
+    return data
+
+
